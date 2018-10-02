9e4b067b0fec8b493f72559d7826dd474dbd9e07
Michel Yoeung <Michel.Yoeung@grenoble-inp.org>
2018-10-02 10:39:47 +0200

    tp sql

--- SGBD/TP1/requetes.sql
+++ SGBD/TP1/requetes.sql
@@ -62,6 +62,15 @@ select H.NS, H.NH, H.nomH, H.adrH, H.catH, count(B.NS)
 from hotels H, bookings B
 where H.NS = B.NS and H.NH = B.NH
 group by H.NS, H.NH, H.nomH, H.adrH, H.catH;
+-- correction --
+select H.NS, H.NH, count(B.NS)
+from hotels H, bookings B
+where H.NS = B.NS and H.NH = B.NH
+group by H.NS, H.NH
+union
+(select H.NS, H.NH, 0
+from hotels H
+where (NS, NH) not in (select NS, NH from bookings));
 
 -- R11 --
 -- no rows selected --
@@ -69,16 +78,46 @@ select H.adrH
 from resorts R, hotels H, bookings B
 where R.NomS = 'Chamonix' and H.NS = R.NS
       and H.NS = B.NS and H.NH = B.NH
-group by H.adrH, H.NS having count(B.NS) = (select max(nb)
-                                            from (select count (B.NS) nb
-                                                  from hotels H, bookings B
-                                                  where H.NS = B.NS and H.NH = B.NH
-                                                  group by H.NS));
+group by H.adrH, H.NS having count(B.NS) = (select max(count(B.NS))
+                                            from hotels H, bookings B
+                                            where H.NS = B.NS and H.NH = B.NH
+                                            group by H.NS));
+-- correction --
+select H.adrH
+from resorts R, hotels H, bookings B
+where R.NomS = 'Chamonix' and H.NS = R.NS
+      and H.NS = B.NS and H.NH = B.NH
+group by H.adrH, H.NS having count(B.NS) = (select max(count(B.NS))
+                                            from resorts R, bookings B
+                                            where R.NS = B.NS and R.NomS = 'Chamonix'
+                                            group by B.NS, B.NH);
+
+-- R12 --
+-- correction --
+select B.jour
+from bookings B, resorts R, hotels H
+where R.NS = H.NS and H.NS = B.NS and H.NH = B.NH
+      and R.NomS = 'Chamonix' and H.NomH = 'Bon Séjour'
+group by B.jour having count(*) = (select max(count(*))
+                                    ...
+                                  );
 
 -- R13 --
--- no rows selected --
+-- no rows selected au lieu de 1 (Sales, Salesman, 5600€, 4, 1400€) --
 select H.NS, H.NH, H.nomH, H.adrH, H.catH
 from hotels H
 where H.NH not in (select distinct H.NH
-                   from hotels H, rooms R
-                   where H.NS = R.NS and H.NH = R.NH and R.prix > 40);
+                   from hotels H1, rooms R
+                   where H1.NS = R.NS and H1.NH = R.NH and R.prix > 40);
+
+-- R15 --
+select G.NCL, G.NomCl -- dividende --
+from guests G
+where not exists (select null -- diviseur --
+                  from hotels H, resorts R
+                  where H.NS = R.NS and R.NomS = 'Chamonix' and CatH = '2'
+                        and not exists (select null -- liaisons --
+                                        from guests G1, bookings B
+                                        where G1.NCL = G.NCL  -- liaison G et G1 --
+                                              and G1.NCL = B.NCL  -- liaison guest et bookings ("clients ayant réservé") --
+                                              and B.NS = H.NS and B.NH = H.NH));  -- liaison bookings et hotels ("réservé dans tous les hôtels")--
